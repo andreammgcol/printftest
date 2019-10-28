4bbee548eb610686fa979cd0edbc6e1358566865
Holberton <holberton@Holbertons-iMac-9.local>
2019-10-28 10:46:10 -0500

    prueba

--- _printf.c
+++ _printf.c
@@ -4,43 +4,44 @@
 
 int _printf(const char *format, ...)
 {
-	va_list arguments;
-	char *p, *sval;
-	int ival;
+   va_list arguments;
+   const char *p;
+   char *str;
+   int x;
 
-	va_start(arguments, format);
-	int num = 0;
-	for (p = format; *p; p++)
-	{
-		if (*p != '%')
-		{
-			putchar(*p);
-			num++;
-			continue;
-		}
-		switch (*++p)
-		{
-		case 's':
-		for (sval = va_arg(arguments, char *); *sval; sval++)
-		{
-		putchar(*sval);
-		num++;
-		}
-		break;
+   va_start(arguments, format);
+   int num = 0;
 
-		case 'c':
-		ival = va_arg(arguments, int);
-		printf("%c", c);
-		num++;
-		break;
-		
-		default:
-		putchar(*p);
-		num++;
-		break;
-		}
-	}
-va_end(arguments);
-
-return (num);
+   for (p = format; *p; p++)
+   {
+      if (*p != '%')
+      {
+         putchar(*p);
+         num++;
+         continue;
+      }
+      switch (*++p)
+      {
+        case 's':
+            for (str = va_arg(arguments, char *); *str; str++)
+	    {
+                printf("%s", *str);
+                num++;
+            }
+            break;
+        case 'c':
+            x = va_arg(arguments, int);
+            printf("%c", x);
+            num++;
+            break;
+        default:
+            putchar(*p);
+            num++;
+            break;
+      }
+     
+   }
+   va_end(arguments);
+   
+   return (num);
 }
